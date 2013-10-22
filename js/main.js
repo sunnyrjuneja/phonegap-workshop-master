4153c8a9d9bbf70f48adb698b606b76704941191
Sunny Juneja <me@sunnyjuneja.com>
2013-10-22 14:41:14 -0700

    Testing to see how alerts look different in native vs browser

--- js/main.js
+++ js/main.js
@@ -13,8 +13,18 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
+    showAlert: function(message, title) {
+      if (navigation.notifcation) {
+          navigator.notification.alert(message, null, title, 'OK');
+      } else { 
+          alert(title ? title + ": " + message) : message);
+      }
+    }
+    initialize: function() { 
+        var self = this;
+        this.store = new MemoryStore(function() {
+          self.showAlert('Store Initialized', 'Info');
+        }
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
